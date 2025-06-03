fabbff6a37cfd2ffb24b7605947881cc2f4f8bcb
Asqalani112 <abedila201@gmail.com>
2025-06-03 15:57:12 +0300

    fix(Purchase Invoice): Add default warehouse field to PI and warehouse to PI Item and apply default warehouse to items

--- accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
@@ -46,7 +46,14 @@ frappe.ui.form.on('Purchase Invoice Item', {
   rate: function(frm, cdt, cdn) {
     calculate_amount(cdt, cdn);
     update_totals(frm);
-  }
+  },
+  items_add: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (frm.doc.default_warehouse) {
+            row.warehouse = frm.doc.default_warehouse;
+            frm.refresh_field("items");
+        }
+    }
 });
 
 function calculate_amount(cdt, cdn) {
