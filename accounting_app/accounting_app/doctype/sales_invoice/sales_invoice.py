a5eccc1348e4838ea4ad6d342ac0e5234127be76
Asqalani112 <131972140+Asqalani112@users.noreply.github.com>
2025-06-19 16:23:01 +0300

    feat(Sales Invoice): Add inventory valuation and stock impact on submission (#20)
    * feat(Sales Invoice): Add inventory valuation and stock impact on submission
    
    * fix(Purchase Invoice): Use item.rate directly for valuation instead of stock aggregation
    
    * fix: move constant accounts to be fetched from warehouse
    
    * fix: improve stock valuation calculation
    
    * refactor: fetch inventory and expense accounts dynamically from warehouse in sales invoice
    
    * bulid: update version
    
    * fix: handle item-wise GL entries and valuation rate based on each item's warehouse
    
    * fix: implement accurate GL entries and stock movements per item and warehouse
    
    * fix(gl-entry): separate valuation amounts by both account and warehouse to prevent incorrect grouping

--- accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
@@ -1,19 +1,21 @@
 # Copyright (c) 2025, asqalani and contributors
 # For license information, please see license.txt
+from collections import defaultdict
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import nowtime
 from ...utils.account_controller import AccountController
+from ...utils.stock_controller import StockController
 
 
 
 
 
 
-class SalesInvoice(Document, AccountController):
+class SalesInvoice(Document, AccountController, StockController):
 	def on_submit(self):
 		entries = []
-
 		# Debit: Customer
 		entries.append({
 			"posting_date": self.posting_date,
@@ -37,10 +39,86 @@ class SalesInvoice(Document, AccountController):
 			"voucher_number": self.name
 		})
 
+		# تجميع القيود حسب الحساب
+		inventory_totals = defaultdict(float)
+		expense_totals = defaultdict(float)
+
+		stock_entries = []
+		#get valution rate
+		for item in self.items:
+			inventory_account = frappe.db.get_value("Warehouse", item.warehouse, "inventory_account")
+			expense_account = frappe.db.get_value("Warehouse", item.warehouse, "expense_account")
+
+			if not self.default_warehouse:
+				frappe.throw(f"Please set Warehouse for item {item.item}")
+
+			data = frappe.db.sql("""
+		               SELECT
+		                   SUM(qty) AS total_qty,
+		                   SUM(stock_value) AS total_value
+		               FROM `tabStock Ledger Entry`
+		               WHERE
+		                   item = %s AND
+		                   warehouse = %s AND
+		                   docstatus = 1 AND
+		                   qty > 0
+
+		           """, (item.item, item.warehouse), as_dict=True)[0]
+
+			total_qty = data.total_qty or 0
+			total_value = data.total_value or 0
+
+			valuation_rate = total_value / total_qty if total_qty else 0
+			valuation_amount = item.qty * valuation_rate
+			stock_entries.append({
+				"posting_date": self.posting_date,
+				"posting_time": nowtime(),
+				"item": item.item,
+				"warehouse": item.warehouse,
+				"qty": -item.qty,
+				"valuation_rate": valuation_rate,
+				"voucher_type": "Sales Invoice",
+				"voucher_no": self.name,
+				"is_cancelled": 0
+			})
+
+			key_inv = (inventory_account, item.warehouse)
+			key_exp = (expense_account, item.warehouse)
+
+			# تجميع القيم
+			inventory_totals[key_inv] += valuation_amount
+			expense_totals[key_exp] += valuation_amount
+
+		for (account, warehouse), value in inventory_totals.items():
+			if value:
+				entries.append({
+					"posting_date": self.posting_date,
+					"due_date": self.payment_due_date,
+					"account": account,
+					"debit_amount": 0,
+					"credit_amount": value,
+					"voucher_type": "Sales Invoice",
+					"voucher_number": self.name,
+				})
+
+		for (account, warehouse), value in expense_totals.items():
+			if value:
+				entries.append({
+					"posting_date": self.posting_date,
+					"due_date": self.payment_due_date,
+					"account": account,
+					"debit_amount": value,
+					"credit_amount": 0,
+					"voucher_type": "Sales Invoice",
+					"voucher_number": self.name,
+				})
+
 		self.make_gl_entries(entries)
+		self.make_stock_ledger_entries(stock_entries)
 
 	def on_cancel(self):
 		self.make_reverse_gl_entries("Sales Invoice", self.name)
+		self.make_reverse_stock_ledger_entries("Sales Invoice", self.name)
 
 	def validate(self):
 		# حساب amount لكل عنصر
